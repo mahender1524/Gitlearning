0daedb86e8e14b77f32f581cef90e69caf96a20c
mahender1524 <mahibisht560@gmail.com>
2020-06-06 00:39:43 +0530

    Git intro

---
+++ GitINTROduction.php
@@ -0,0 +1,56 @@
+Git
+
+
+=====================>
+
+
+Git is distrubuted version control system ( )
+
+Git is a free and open source software
+
+ 
+git --version
+
+
+git config --global user.email "--------------" 
+git config  --global user.password "----------------"
+git clear
+
+
+cd f:
+mkdir testmahi
+cd testmahi
+
+
+Crate file using command
+
+dir
+touch index.html
+
+dir
+
+notepad index.php file open ctrl+s close file
+
+
+GIT ADDDD 
+
+git add.
+git add -A
+
+GIT COMMIT (Flag or a message )
+
+git commit -m "file added "
+git log (display the id where id is encrypt format )
+commit 12b53b9e88c927d5c4487020057e7679c8a0bc65 (HEAD -> master)
+
+
+6 digit code display 
+
+ git log --oneline
+12b53b9 (HEAD -> master) index file added
+
+GITHUB==============================================================================> cloud coumputing and hosting a code 
+
+
+code ko host krne k liye ek platefrom prived krta hai
+cloud or a data center
